5654cf8b83192741fd9ebcff870a684dcaf9931b
jaiver <javer.uzc@gmail.com>
2025-09-26 16:59:57 -0400

    ADDED:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,25 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            load: [ EnvConfiguration ]
+        }),
+
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '..', 'public'),
         }),
 
-        MongooseModule.forRoot('mongodb://localhost:27017/nest-pokedex'),
+        MongooseModule.forRoot( process.env.MONGODB || 'mongodb://localhost:27017/{name_bd}' ),
 
         PokemonModule,
 
